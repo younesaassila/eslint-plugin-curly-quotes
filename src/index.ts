f238ad489d28e6d6cceb2b221f929f234fc63249
younesaassila <47226184+younesaassila@users.noreply.github.com>
2024-11-08 21:42:59 +0100

    Remove misplaced schema

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,6 @@ export default {
   meta: {
     name,
     version,
-    schema: [
-      {
-        type: "object",
-        properties: {
-          "single-opening": { type: "string" },
-          "single-closing": { type: "string" },
-          "double-opening": { type: "string" },
-          "double-closing": { type: "string" },
-          "ignored-jsx-elements": { type: "array", items: { type: "string" } },
-          "ignored-jsx-attributes": {
-            type: "array",
-            items: { type: "string" },
-          },
-          "ignored-function-calls": {
-            type: "array",
-            items: { type: "string" },
-          },
-        },
-        additionalProperties: false,
-      },
-    ],
   },
   rules: {
     "no-straight-quotes": noStraightQuotes,
