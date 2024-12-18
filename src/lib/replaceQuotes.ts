c405b42b18d5270d901588d9becd6d190d6d0ee4
younesaassila <47226184+younesaassila@users.noreply.github.com>
2024-12-18 11:57:35 +0100

    Improve var names & comments

--- src/lib/replaceQuotes.ts
+++ src/lib/replaceQuotes.ts
@@ -18,13 +18,13 @@ export default function replaceQuotes(
   closingCharacter: string,
   ignoredIndexRanges: IgnoredIndexRange[] = []
 ): string {
-  const quoteRegex = new RegExp(straightCharacter, "g")
-  const matches = [
+  const straightRegex = new RegExp(straightCharacter, "g")
+  const straightMatches = [
     ...text
       .substring(textTrimValue, text.length - textTrimValue)
-      .matchAll(quoteRegex),
+      .matchAll(straightRegex),
   ]
-  const indices = matches
+  const straightIndices = straightMatches
     .map(match => match.index + textTrimValue)
     .filter(index =>
       ignoredIndexRanges.every(range => index < range[0] || range[1] <= index)
@@ -44,10 +44,11 @@ export default function replaceQuotes(
   }
 
   // Adapted from https://stackoverflow.com/questions/509685/ideas-for-converting-straight-quotes-to-curly-quotes
-  for (let i = 0; i < indices.length; i++) {
-    const index = indices[i]
-    const previousChar = index > 0 ? textChars[index - 1] : ""
-    const nextChar = index < textChars.length - 1 ? textChars[index + 1] : ""
+  for (let i = 0; i < straightIndices.length; i++) {
+    const straightIndex = straightIndices[i]
+    const previousChar = straightIndex > 0 ? textChars[straightIndex - 1] : ""
+    const nextChar =
+      straightIndex < textChars.length - 1 ? textChars[straightIndex + 1] : ""
 
     const isOpeningCharacter =
       isWhitespaceOrEmpty(previousChar) && !isWhitespaceOrEmpty(nextChar)
@@ -55,21 +56,23 @@ export default function replaceQuotes(
       !isWhitespaceOrEmpty(previousChar) && isWhitespaceOrEmpty(nextChar)
     const isApostrophe =
       straightCharacter === "'" &&
-      ((!isWhitespaceOrEmpty(previousChar) && index !== textTrimValue) || // Is not at beginning of text nor preceded by whitespace character
+      ((!isWhitespaceOrEmpty(previousChar) &&
+        straightIndex !== textTrimValue) || // Is not at beginning of text nor preceded by whitespace character
         text.length - 2 * textTrimValue === 1) // Is only character
 
-    if (isOpeningCharacter) setOpeningCharacter(index)
-    else if (isClosingCharacter) setClosingCharacter(index)
-    else if (isApostrophe) setClosingCharacter(index, false)
+    if (isOpeningCharacter) setOpeningCharacter(straightIndex)
+    else if (isClosingCharacter) setClosingCharacter(straightIndex)
+    else if (isApostrophe) setClosingCharacter(straightIndex, false)
     else {
-      if (openedQuotes === 0) setOpeningCharacter(index)
-      else setClosingCharacter(index)
+      if (openedQuotes === 0) setOpeningCharacter(straightIndex)
+      else setClosingCharacter(straightIndex)
     }
 
-    const segmentStart = i > 0 ? indices[i - 1] : 0
-    const segment = text.substring(segmentStart, index)
+    // Check for backslashes between last straight quote and current one.
+    const segmentStart = i > 0 ? straightIndices[i - 1] : 0
+    const segment = text.substring(segmentStart, straightIndex)
     if (isEscapedQuote(segment)) {
-      escapedQuotesIndices.push(index - 1)
+      escapedQuotesIndices.push(straightIndex - 1)
     }
   }
 
